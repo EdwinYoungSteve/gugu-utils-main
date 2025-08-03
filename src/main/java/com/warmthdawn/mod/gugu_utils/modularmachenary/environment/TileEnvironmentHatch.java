446c06284a967a3189745ae4de87430047399267
mc_Edwin <yxy2439642228@163.com>
2025-08-04 00:06:20 +0800

    Revert "增加了对于MMCEAddons环境仓的集成"
    This reverts commit 34993cb5e682dbf37d15a5dfa8cd23a76d4cc08b.

--- src/main/java/com/warmthdawn/mod/gugu_utils/modularmachenary/environment/TileEnvironmentHatch.java
+++ src/main/java/com/warmthdawn/mod/gugu_utils/modularmachenary/environment/TileEnvironmentHatch.java
@@ -6,32 +6,15 @@ import com.warmthdawn.mod.gugu_utils.modularmachenary.requirements.RequirementEn
 import com.warmthdawn.mod.gugu_utils.modularmachenary.requirements.basic.CraftingResourceHolder;
 import com.warmthdawn.mod.gugu_utils.modularmachenary.requirements.basic.IConsumable;
 import hellfirepvp.modularmachinery.common.crafting.ComponentType;
-import hellfirepvp.modularmachinery.common.crafting.helper.CraftCheck;
 import hellfirepvp.modularmachinery.common.machine.IOType;
 import hellfirepvp.modularmachinery.common.machine.MachineComponent;
 import hellfirepvp.modularmachinery.common.tiles.base.MachineComponentTile;
-import net.minecraft.world.biome.Biome;
 
 import javax.annotation.Nullable;
-import java.lang.reflect.Method;
-import java.util.Objects;
 
 import static com.warmthdawn.mod.gugu_utils.common.Constants.STRING_RESOURCE_ENVIRONMENT;
 
 public class TileEnvironmentHatch extends CommonMMTile implements IConsumable<RequirementEnvironment.RT>, MachineComponentTile {
-
-    // Check if MMCE Addons is available at runtime
-    private static final boolean MMCE_ADDONS_AVAILABLE = checkMMCEAddonsAvailable();
-    
-    private static boolean checkMMCEAddonsAvailable() {
-        try {
-            Class.forName("github.alecsio.mmceaddons.common.crafting.requirement.RequirementDimension");
-            Class.forName("github.alecsio.mmceaddons.common.crafting.requirement.RequirementBiome");
-            return true;
-        } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
 
     @Nullable
     @Override
@@ -44,7 +27,7 @@ public class TileEnvironmentHatch extends CommonMMTile implements IConsumable<Re
 
             @Override
             public Object getContainerProvider() {
-                return new CompatibilityWrapper();
+                return new CraftingResourceHolder<>(TileEnvironmentHatch.this);
             }
         };
     }
@@ -58,69 +41,5 @@ public class TileEnvironmentHatch extends CommonMMTile implements IConsumable<Re
         return false;
     }
 
-    /**
-     * Compatibility wrapper that can handle both GuGu Utils and MMCE Addons requirements
-     */
-    private class CompatibilityWrapper extends CraftingResourceHolder<RequirementEnvironment.RT> {
-        
-        public CompatibilityWrapper() {
-            super(TileEnvironmentHatch.this);
-        }
-        
-        // MMCE Addons dimension requirement handling (via reflection)
-        public CraftCheck canHandle(Object requirement) {
-            if (!MMCE_ADDONS_AVAILABLE) return CraftCheck.failure("MMCE Addons not available");
-            
-            String className = requirement.getClass().getSimpleName();
-            
-            if ("RequirementDimension".equals(className)) {
-                return handleDimensionRequirement(requirement);
-            } else if ("RequirementBiome".equals(className)) {
-                return handleBiomeRequirement(requirement);
-            }
-            
-            return CraftCheck.failure("Unknown requirement type: " + className);
-        }
-        
-        public void handle(Object requirement) {
-            // No-op, just like MMCE Addons implementation
-        }
-        
-        private CraftCheck handleDimensionRequirement(Object requirement) {
-            try {
-                // Use reflection to get dimension ID from RequirementDimension
-                Method getDimensionMethod = requirement.getClass().getMethod("getDimension");
-                Object dimension = getDimensionMethod.invoke(requirement);
-                Method getIdMethod = dimension.getClass().getMethod("getId");
-                int dimensionId = (Integer) getIdMethod.invoke(dimension);
-                
-                // Check if current dimension matches
-                return TileEnvironmentHatch.this.world.provider.getDimension() == dimensionId ? 
-                    CraftCheck.success() : 
-                    CraftCheck.failure("error.modularmachineryaddons.requirement.missing.dimension");
-            } catch (Exception e) {
-                return CraftCheck.failure("Error checking dimension requirement: " + e.getMessage());
-            }
-        }
-        
-        private CraftCheck handleBiomeRequirement(Object requirement) {
-            try {
-                // Use reflection to get biome registry name from RequirementBiome
-                Method getBiomeMethod = requirement.getClass().getMethod("getBiome");
-                Object biome = getBiomeMethod.invoke(requirement);
-                Method getRegistryNameMethod = biome.getClass().getMethod("getRegistryName");
-                String requiredBiomeRegistryName = (String) getRegistryNameMethod.invoke(biome);
-                
-                // Check if current biome matches
-                Biome currentBiome = TileEnvironmentHatch.this.world.getBiome(TileEnvironmentHatch.this.getPos());
-                String currentBiomeRegistryName = Objects.requireNonNull(currentBiome.getRegistryName()).toString();
-                
-                return currentBiomeRegistryName.equalsIgnoreCase(requiredBiomeRegistryName) ? 
-                    CraftCheck.success() : 
-                    CraftCheck.failure("error.modularmachineryaddons.requirement.missing.biome");
-            } catch (Exception e) {
-                return CraftCheck.failure("Error checking biome requirement: " + e.getMessage());
-            }
-        }
-    }
+
 }
